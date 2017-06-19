d0fcec2d64f4542c5482d685df354ec753879acd
Allie Caldwell <alison.jane.c@gmail.com>
2017-06-19 13:06:54 -0400

    first commit

--- app/src/main/java/com/alisonjc/compmusicplayer/playlists/OnPlaylistInteractionListener.java
+++
@@ -1,6 +0,0 @@
-package com.alisonjc.compmusicplayer.playlists;
-
-
-public interface OnPlaylistInteractionListener {
-    void onPlaylistSelected(String userId, String playlistId, String playlistTitle);
-}
