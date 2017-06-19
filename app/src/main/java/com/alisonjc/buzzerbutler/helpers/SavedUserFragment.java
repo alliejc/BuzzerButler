91d01fb07e5f7a2825146d2b257187ddb7b5d3c2
cbonoz <chrisdistrict@gmail.com>
2017-06-19 14:32:15 -0400

    update build.gradle

--- app/src/main/java/com/alisonjc/buzzerbutler/helpers/SavedUserFragment.java
+++ app/src/main/java/com/alisonjc/buzzerbutler/helpers/SavedUserFragment.java
@@ -1,4 +1,4 @@
-package com.alisonjc.buzzerbutler;
+package com.alisonjc.buzzerbutler.helpers;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.alisonjc.buzzerbutler.R;
 
 
 /**
