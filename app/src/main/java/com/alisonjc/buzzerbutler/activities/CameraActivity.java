8f8d76ff139cca231c6c7cf927b1d886000575f6
cbonoz <chrisdistrict@gmail.com>
2017-06-19 16:53:49 -0400

    add camera activity logic

--- app/src/main/java/com/alisonjc/buzzerbutler/activities/CameraActivity.java
+++ app/src/main/java/com/alisonjc/buzzerbutler/activities/CameraActivity.java
@@ -1,15 +1,210 @@
 package com.alisonjc.buzzerbutler.activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.LayoutParams;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.alisonjc.buzzerbutler.R;
+import com.alisonjc.buzzerbutler.helpers.CompareFaces;
+import com.alisonjc.buzzerbutler.helpers.PrefManager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static com.alisonjc.buzzerbutler.R.id.email;
 
 public class CameraActivity extends AppCompatActivity {
 
+    private static final String TAG = "CamTestActivity";
+    Preview preview;
+    Button buttonClick;
+    Camera camera;
+    Activity act;
+    Context ctx;
+
+    private String email;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ctx = this;
+        act = this;
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_camera);
+
+        email = PrefManager.getInstance().getString("email", null);
+
+        preview = new Preview(this, (SurfaceView) findViewById(R.id.surfaceView));
+        preview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        ((FrameLayout) findViewById(R.id.layout)).addView(preview);
+        preview.setKeepScreenOn(true);
+
+        preview.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+            }
+        });
+
+        Toast.makeText(ctx, "Take personal photo to complete login", Toast.LENGTH_LONG).show();
+
+        //		buttonClick = (Button) findViewById(R.id.btnCapture);
+        //
+        //		buttonClick.setOnClickListener(new OnClickListener() {
+        //			public void onClick(View v) {
+        ////				preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        //				camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        //			}
+        //		});
+        //
+        //		buttonClick.setOnLongClickListener(new OnLongClickListener(){
+        //			@Override
+        //			public boolean onLongClick(View arg0) {
+        //				camera.autoFocus(new AutoFocusCallback(){
+        //					@Override
+        //					public void onAutoFocus(boolean arg0, Camera arg1) {
+        //						//camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        //					}
+        //				});
+        //				return true;
+        //			}
+        //		});
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int numCams = Camera.getNumberOfCameras();
+        if (numCams > 0) {
+            try {
+                camera = Camera.open(0);
+                camera.startPreview();
+                preview.setCamera(camera);
+            } catch (RuntimeException ex) {
+                Toast.makeText(ctx, "Camera not found", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (camera != null) {
+            camera.stopPreview();
+            preview.setCamera(null);
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
+    private void resetCam() {
+        camera.startPreview();
+        preview.setCamera(camera);
+    }
+
+    private void refreshGallery(File file) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(Uri.fromFile(file));
+        sendBroadcast(mediaScanIntent);
+    }
+
+    ShutterCallback shutterCallback = new ShutterCallback() {
+        public void onShutter() {
+            //			 Log.d(TAG, "onShutter'd");
+        }
+    };
+
+    PictureCallback rawCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            //			 Log.d(TAG, "onPictureTaken - raw");
+        }
+    };
+
+    PictureCallback jpegCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            Log.d(TAG, "onPictureTaken - jpeg");
+            // TODO: implement this comparison to see if the photo is still set.
+            final byte[] existingPhoto = data;
+            if (existingPhoto != null) {
+                // Verify the photo for the correct user.
+                CompareFaces.verifyPhoto(data);
+                resetCam();
+                Log.d(TAG, "onPictureTaken - jpeg");
+            } else {
+                new SaveImageTask().execute(data);
+            }
+
+        }
+    };
+
+    private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+
+        @Override
+        protected Void doInBackground(byte[]... data) {
+            FileOutputStream outStream = null;
+
+            // Write to SD Card
+            try {
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File(sdCard.getAbsolutePath() + "/camtest");
+                dir.mkdirs();
+
+                String fileName = String.format("%s.jpg", email);
+                File outFile = new File(dir, fileName);
+
+                outStream = new FileOutputStream(outFile);
+                outStream.write(data[0]);
+                outStream.flush();
+                outStream.close();
+
+                Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length + " to " + outFile.getAbsolutePath());
+
+                refreshGallery(outFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+            }
+            return null;
+        }
+
     }
 }
+
